9385033b64dc3fdbe85ad16736ee41c52bd4280a
Ruslan Karpov <rkarpov88@gmail.com>
2023-05-19 09:16:08 +0300

    finished task 04-auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,20 +4,26 @@ const ctrl = require("../../controllers/contacts-controller");
 
 const { validateBody } = require("../../decorators");
 
-const isValidId = require("../../middlewares/isValidId");
+const { isValidId, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.listContacts);
+router.get("/", authenticate, ctrl.listContacts);
 
-router.get("/:id", isValidId, ctrl.getContactById);
+router.get("/:id", authenticate, isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
@@ -25,11 +31,12 @@ router.patch(
 
 router.put(
   "/:id",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateContact
 );
 
-router.delete("/:id", ctrl.removeContact);
+router.delete("/:id", authenticate, ctrl.removeContact);
 
 module.exports = router;
