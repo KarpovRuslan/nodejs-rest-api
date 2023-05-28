a8e02f90d90ce43bf7a983d0486ae0778b3d99fd
Ruslan Karpov <rkarpov88@gmail.com>
2023-05-28 22:20:12 +0300

    finished hw05-avatars

---
+++ login.test.js
@@ -0,0 +1,47 @@
+/*
+1 - відповідь повина мати статус-код 200
+2 - у відповіді повинен повертатися токен
+3 - у відповіді повинен повертатися об'єкт user з 2 полями email и subscription з типом даних String
+*/
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+const request = require("supertest");
+const app = require("./app");
+const { User } = require("./models/user");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+describe("test Login", () => {
+  let server;
+  beforeAll(() => (server = app.listen(PORT)));
+  afterAll(() => server.close());
+  beforeEach((done) => {
+    mongoose.connect(DB_HOST).then(() => done());
+  });
+  afterEach((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      mongoose.connection.close(() => done());
+    });
+  });
+
+  test("testing User Login", async () => {
+    const testUser = {
+      email: "testemail@gmail.com",
+      password: "testemail",
+    };
+
+    const responseLogin = await request(app)
+      .post("/api/users/login")
+      .send(testUser);
+
+    const user = await User.findOne({ email: testUser.email });
+
+    expect(responseLogin.statusCode).toBe(200);
+    expect(responseLogin.body.token).toBeTruthy();
+    const { token } = await User.findById(user._id);
+    expect(responseLogin.body.token).toBe(token);
+    expect(responseLogin.body.user.email).toBe(testUser.email);
+    expect(responseLogin.body.user.subscription).toBe(user.subscription);
+  });
+});
